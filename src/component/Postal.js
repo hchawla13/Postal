93c802ceb3ba9f95d64562c9d4088a739839ac1c
US\hchawla <HarpreetSingh.Chawla@team.telstra.com>
2019-11-16 20:00:34 +0530

    rendering on a table

--- src/component/Postal.js
+++ src/component/Postal.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+const axios = require('axios');
 
 
 
 class Postal extends Component {
+    state={
+        locationList:[]
+    }
+    
+    componentDidMount() {
+        axios.get('https://api.myjson.com/bins/123jsq')
+        .then((response) =>{
+            console.log("response",response.data)
+            this.setState({locationList:response.data})
+            console.log("this.state",this.state.locationList)
+        })
+        .catch((err)=>{
+            console.log("error",err)
+        })
+    }
     
     render() {
+        const data = this.state.locationList
+        console.log("this.state.locationList",this.state.locationList)
+        const columns = [{
+            Header: 'Postal Code',
+            accessor: 'postal_code' // String-based value accessors!
+          }, {
+            Header: 'Latitude',
+            accessor: 'latitude',
+          }, {
+            Header: 'Longitude',
+            accessor: 'longitude'
+          }]
         return (
-            <div className="postal">
-                Postal Component
-            </div>
+            <ReactTable
+                data={data}
+                columns={columns}
+            />
         )
     }
 }
