ba9f70ad49d581c91cae13b791dca059e3014527
US\hchawla <HarpreetSingh.Chawla@team.telstra.com>
2019-11-16 20:47:57 +0530

    displaying 2 items on map

--- src/component/Postal.js
+++ src/component/Postal.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
+import shouldPureComponentUpdate from 'react-pure-render';
+import PropTypes from 'prop-types';
+import MyGreatPlace from './MyGreatPlace';
+import GoogleMap from 'google-map-react';
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 const axios = require('axios');
+//https://api.myjson.com/bins/fikz6 five addresses with names
+// https://api.myjson.com/bins/123jsq 1000 records
 
 
 
 class Postal extends Component {
+    static propTypes = {
+        center: PropTypes.array,
+        zoom: PropTypes.number,
+        greatPlaceCoords: PropTypes.any
+      };
+      static defaultProps = {
+        center: [59.938043, 30.337157],
+        zoom: 9,
+        greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+      };
+      shouldComponentUpdate = shouldPureComponentUpdate;
     state={
         locationList:[]
     }
     
     componentDidMount() {
-        axios.get('https://api.myjson.com/bins/123jsq')
+        axios.get('https://api.myjson.com/bins/fikz6')
         .then((response) =>{
             console.log("response",response.data)
             this.setState({locationList:response.data})
@@ -23,23 +40,23 @@ class Postal extends Component {
     }
     
     render() {
-        const data = this.state.locationList
-        console.log("this.state.locationList",this.state.locationList)
-        const columns = [{
-            Header: 'Postal Code',
-            accessor: 'postal_code' // String-based value accessors!
-          }, {
-            Header: 'Latitude',
-            accessor: 'latitude',
-          }, {
-            Header: 'Longitude',
-            accessor: 'longitude'
-          }]
+        
         return (
-            <ReactTable
-                data={data}
-                columns={columns}
-            />
+            <div style={{position:'absolute',width:'100%',height:'100%'}}>
+                <GoogleMap
+                    center={this.props.center}
+                    zoom={this.props.zoom}>
+                    <MyGreatPlace lat={59.955413} lng={30.337844} text={'A'} />
+                    <MyGreatPlace {...this.props.greatPlaceCoords} text={'B'}  />
+                </GoogleMap>
+                {/* {
+                    this.state.locationList.map((item)=>{
+                        return (
+                        <li key={item.id}>{item.name}</li>
+                        )
+                    })
+                } */}
+            </div>
         )
     }
 }
