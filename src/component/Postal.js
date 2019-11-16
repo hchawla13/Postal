b84606769f39ec95c36791afa2e02c81e7c2906e
US\hchawla <HarpreetSingh.Chawla@team.telstra.com>
2019-11-16 15:10:49 +0530

    init commit

---
+++ src/component/Postal.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+
+class Postal extends Component {
+    
+    render() {
+        return (
+            <div className="postal">
+                Postal Component
+            </div>
+        )
+    }
+}
+
+export default Postal;
